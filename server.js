54f5a9e23ce41759d81c3986ebef7815d96ce9c3
merfat <71697060+meroOMohammed@users.noreply.github.com>
2025-03-19 19:54:48 +0200

    Updated server.js and project dependencies

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.post('/data', (req, res) => {
+    res.json({ receivedData: req.body });
+});
+
+app.use((req, res) => {
+    res.status(404).json({ error: 'Page not found' });
 });
 
 app.listen(port, () => {
